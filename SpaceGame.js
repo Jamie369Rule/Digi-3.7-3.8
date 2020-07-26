2a1857e303b7d7a85b938d210dc45771793632ab
Jamie369Rule <65513395+Jamie369Rule@users.noreply.github.com>
2020-07-27 10:47:01 +1200

    27/7 Created the game canvas

--- SpaceGame.js
+++ SpaceGame.js
@@ -1,1 +1,8 @@
-console.log("practice");
+const cvs = document.getElementById("canvas");
+const ctx = cvs.getContext("2d");
+
+// color canvas
+ctx.beginPath();
+ctx.rect(0, 0, 800, 500);
+ctx.fillStyle = "black";
+ctx.fill();
