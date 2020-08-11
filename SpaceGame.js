76943891fec3d9309ed8a6ad02ae5cfa83ad825d
Jamie369Rule <65513395+Jamie369Rule@users.noreply.github.com>
2020-08-11 14:56:45 +1200

    Created a circle to use as Spaceship

--- SpaceGame.js
+++ SpaceGame.js
@@ -1,8 +1,45 @@
-const cvs = document.getElementById("canvas");
+const cvs = document.getElementById("spaceGame");
 const ctx = cvs.getContext("2d");
 
 // color canvas
 ctx.beginPath();
 ctx.rect(0, 0, 800, 500);
-ctx.fillStyle = "black";
+ctx.fillStyle = "Black";
 ctx.fill();
+
+//variables and constants
+const ballX = 75
+const ballRadius = 15
+
+
+// this ball is my practice spaceship
+// create ball
+const ball = {
+	x : ballX,
+	y : cvs.height/2,
+	radius : ballRadius,
+}
+
+// draw ball
+function drawBall(){
+  ctx.beginPath();
+
+  ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI*2);
+  ctx.fillStyle = "White";
+  ctx.fill();
+
+  ctx.closePath();
+}
+
+//game loop
+function loop(){
+  ctx.beginPath();
+  ctx.rect(0, 0, 800, 500);
+  ctx.fillStyle = "Black";
+  ctx.fill();
+
+  drawBall();
+
+		requestAnimationFrame(loop);
+	}
+loop();
