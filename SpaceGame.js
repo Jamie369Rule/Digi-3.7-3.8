96fea942b25e5e811fd13193e2bf2cb1f6294117
Jamie369Rule <65513395+Jamie369Rule@users.noreply.github.com>
2020-08-25 14:35:37 +1200

    Some type of movement for the obstacle.
    I have created some simple movement mechanics for the obstacles by creating three separate ones with all there own code. I wanted to do it with an array but couldn't get it to work and was taking too long. So for the moment, I have done it like this.

--- SpaceGame.js
+++ SpaceGame.js
@@ -9,16 +9,16 @@ ctx.fill();
 
 //variables and constants
 const shipX = 75
-const shipRadius = 15
+const shipRadius = 20
 var shipSpeed = 5
 var upArrow = false
 var downArrow = false
-var obsRadius = 30
-var obsSpeed = 5
+var obsRadius = 50
+var obsSpeed = 3
 var obsMinY = 0 + obsRadius;
 var obsMaxY = cvs.height - obsRadius;
-
-
+var obsMinX = cvs.width + shipRadius;
+var obsMaxX = cvs.width + 500;
 
 // this ball is my practice spaceship
 // create spaceship
@@ -65,18 +65,52 @@ function moveShip(){
 }
 
 //create obstacle
-const obs = {
-  x : cvs.width,
-  y : obsY(obsMinY, obsMaxY),
-  radius : obsRadius,
-  speed : obsSpeed
-}
+  const obs1 = {
+    x : obsX(obsMinX, obsMaxX),
+    y : obsY(obsMinY, obsMaxY),
+    radius : obsRadius,
+    speed : obsSpeed
+  }
 
-// draw obstacle
-function drawObs(){
+  const obs2 = {
+    x : obsX(obsMinX, obsMaxX),
+    y : obsY(obsMinY, obsMaxY),
+    radius : obsRadius,
+    speed : obsSpeed
+  }
+
+  const obs3 = {
+    x : obsX(obsMinX, obsMaxX),
+    y : obsY(obsMinY, obsMaxY),
+    radius : obsRadius,
+    speed : obsSpeed
+  }
+
+// draw obstacles
+function drawObs1(){
   ctx.beginPath();
 
-  ctx.arc(obs.x, obs.y, obs.radius, 0, Math.PI*2);
+  ctx.arc(obs1.x, obs1.y, obs1.radius, 0, Math.PI*2);
+  ctx.fillStyle = "White";
+  ctx.fill();
+
+  ctx.closePath();
+}
+
+function drawObs2(){
+  ctx.beginPath();
+
+  ctx.arc(obs2.x, obs2.y, obs2.radius, 0, Math.PI*2);
+  ctx.fillStyle = "White";
+  ctx.fill();
+
+  ctx.closePath();
+}
+
+function drawObs3(){
+  ctx.beginPath();
+
+  ctx.arc(obs3.x, obs3.y, obs3.radius, 0, Math.PI*2);
   ctx.fillStyle = "White";
   ctx.fill();
 
@@ -88,24 +122,51 @@ function obsY(min, max) {
   return Math.random() * (max - min) + min;
 }
 
+//change the X vaule when the ball resets so they come at different times
+function obsX(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
 // move obstacle
-function moveObs() {
-  if(obs.x + obsRadius > 0){
-  obs.x -= obsSpeed;
+function moveObs1() {
+  if(obs1.x + obsRadius > 0){
+  obs1.x -= obsSpeed;
 }else{
-  obs.y = obsY(obsMinY, obsMaxY);
-  obs.x = cvs.width + obsRadius
+  obs1.y = obsY(obsMinY, obsMaxY);
+  obs1.x = obsX(obsMinX, obsMaxX);
   }
 }
 
-function draw(){
-    drawShip();
-    drawObs();
+function moveObs2() {
+  if(obs2.x + obsRadius > 0){
+  obs2.x -= obsSpeed;
+}else{
+  obs2.y = obsY(obsMinY, obsMaxY);
+  obs2.x = obsX(obsMinX, obsMaxX);
+  }
+}
+
+function moveObs3() {
+  if(obs3.x + obsRadius > 0){
+  obs3.x -= obsSpeed;
+}else{
+  obs3.y = obsY(obsMinY, obsMaxY);
+  obs3.x = obsX(obsMinX, obsMaxX);
+  }
 }
 
 function update() {
   moveShip();
-  moveObs();
+  moveObs1();
+  moveObs2();
+  moveObs3();
+}
+
+function draw(){
+    drawShip();
+    drawObs1();
+    drawObs2();
+    drawObs3();
 }
 
 //game loop
@@ -115,9 +176,9 @@ function loop(){
   ctx.fillStyle = "Black";
   ctx.fill();
 
-  draw();
   update();
+  draw();
 
-		requestAnimationFrame(loop);
+  requestAnimationFrame(loop);
 	}
 loop();
