63b59b8376c379b4185e244b7b290f3231873286
Jamie369Rule <65513395+Jamie369Rule@users.noreply.github.com>
2020-09-11 11:12:24 +1200

    Collision detection
    I have added collision detection but at this stage, it does nothing besides showing up in the console.

--- SpaceGame.js
+++ SpaceGame.js
@@ -3,44 +3,31 @@ const ctx = cvs.getContext("2d");
 
 // color canvas
 ctx.beginPath();
-ctx.rect(0, 0, 800, 500);
+ctx.rect(0, 0, cvs.width, cvs.height);
 ctx.fillStyle = "Black";
 ctx.fill();
 
-//variables and constants
-const shipX = 75
-const shipRadius = 15
-var shipSpeed = 4
+
 var upArrow = false
 var downArrow = false
-//var obsRadius = Math.random () * (40 - 20) + 20;
-var obsSpeed = 3
-var obs1MinY = 0 + obsRadius;
-var obs1MaxY = 100 - obsRadius;
-var obs2MinY = 100 + obsRadius;
-var obs2MaxY = 200 - obsRadius;
-var obs3MinY = 200 + obsRadius;
-var obs3MaxY = 300 - obsRadius;
-var obsMinX = cvs.width + shipRadius;
-var obsMaxX = cvs.width + 500;
 
-// var small = 20
-// var medium = 30
-// var large = 40
-var obsRadius = [20, 30, 40];
-var randomRadius = obsRadius[Math.floor(Math.random()*obsRadius.length)];
-console.log("randomRadius")
-
-//var randomRadius = obsRadius[Math.round(Math.random * obsRadius.length)];
+// Obstacle varibales
+var obsRadius = 30
+var obstacles = [];
+var startPos = true
+var totalObs = 6
+var obsMinSpeed = 5
+var obsMaxSpeed = 8
 
 
 
 // this ball is my practice spaceship
 // create spaceship
 const ship = {
-	x : shipX,
+	x : 100,
 	y : cvs.height/2,
-	radius : shipRadius,
+	speed : 4,
+	radius : 15,
 }
 
 // draw ball
@@ -48,7 +35,7 @@ function drawShip(){
   ctx.beginPath();
 
   ctx.arc(ship.x, ship.y, ship.radius, 0, Math.PI*2);
-  ctx.fillStyle = "White";
+  ctx.fillStyle = "Green";
   ctx.fill();
 
   ctx.closePath();
@@ -72,134 +59,74 @@ document.addEventListener("keyup", function(event){
 
 // move the spaceship
 function moveShip(){
-  if(upArrow && ship.y - shipRadius > 0){
-    ship.y -= shipSpeed;
-  }else if(downArrow && ship.y + shipRadius < cvs.height){
-    ship.y += shipSpeed;
+  if(upArrow && ship.y - ship.radius > 0){
+    ship.y -= ship.speed;
+  }else if(downArrow && ship.y + ship.radius < cvs.height){
+    ship.y += ship.speed;
   }
 }
 
-//create obstacle
-  const obs1 = {
-    x : obsX(obsMinX, obsMaxX),
-    y : obsY(obs1MinY, obs1MaxY),
-    radius : randomRadius,
-    speed : obsSpeed
+// Obstacle
+function makeObs() {
+  var obs = {
+    x : cvs.width + obsRadius,
+    y :  Math.floor(Math.random() * ((cvs.height + obsRadius) - obsRadius)),
+    speed : Math.floor(Math.random() * (obsMaxSpeed - obsMinSpeed) + obsMinSpeed),
+		radius : obsRadius
   }
-
-  const obs2 = {
-    x : obsX(obsMinX, obsMaxX),
-    y : obsY(obs2MinY, obs2MaxY),
-    radius : randomRadius,
-    speed : obsSpeed
-  }
-
-  const obs3 = {
-    x : obsX(obsMinX, obsMaxX),
-    y : obsY(obs3MinY, obs3MaxY),
-    radius : randomRadius,
-    speed : obsSpeed
-  }
+    obstacles.push(obs);
+}
 
 // draw obstacles
-function drawObs1(){
-  ctx.beginPath();
-
-  ctx.arc(obs1.x, obs1.y, obs1.radius, 0, Math.PI*2);
-  ctx.fillStyle = "White";
-  ctx.fill();
-
-  ctx.closePath();
+function drawObs(){
+  obstacles.forEach(function(obs, i) {
+    ctx.beginPath();
+    ctx.arc(obs.x, obs.y, obs.radius, 0, Math.PI*2);
+    ctx.fillStyle = "Red";
+    ctx.fill();
+    ctx.closePath();
+  });
 }
 
-function drawObs2(){
-  ctx.beginPath();
 
-  ctx.arc(obs2.x, obs2.y, obs2.radius, 0, Math.PI*2);
-  ctx.fillStyle = "White";
-  ctx.fill();
+function moveObs() {
+  obstacles.forEach(function(obs){
 
-  ctx.closePath();
-}
+    obs.x -= obs.speed;
 
-function drawObs3(){
-  ctx.beginPath();
-
-  ctx.arc(obs3.x, obs3.y, obs3.radius, 0, Math.PI*2);
-  ctx.fillStyle = "White";
-  ctx.fill();
-
-  ctx.closePath();
-}
-
-// change the Y value for the obstacle when it spawns again
-function obsY(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-//change the X vaule when the ball resets so they come at different times
-function obsX(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-//change the raidus of the obs when it spawns again
-
-
-// move obstacle
-function moveObs1() {
-  if(obs1.x + obsRadius > 0){
-  obs1.x -= obsSpeed;
-}else{
-  obs1.y = obsY(obs1MinY, obs1MaxY);
-  obs1.x = obsX(obsMinX, obsMaxX);
-  obs1.radius = randomRadius;
-  }
-}
-
-function moveObs2() {
-  if(obs2.x + obsRadius > 0){
-  obs2.x -= obsSpeed;
-}else{
-  obs2.y = obsY(obs2MinY, obs2MaxY);
-  obs2.x = obsX(obsMinX, obsMaxX);
-  obs2.radius = randomRadius;
-  }
-}
-
-function moveObs3() {
-  if(obs3.x + obsRadius > 0){
-  obs3.x -= obsSpeed;
-}else{
-  obs3.y = obsY(obs2MinY, obs2MaxY);
-  obs3.x = obsX(obsMinX, obsMaxX);
-  obs3.radius = randomRadius;
-  }
-}
-
-function update() {
-  moveShip();
-  moveObs1();
-  moveObs2();
-  moveObs3();
-}
-
-function draw(){
-    drawShip();
-    drawObs1();
-    drawObs2();
-    drawObs3();
+    if(obs.x < 0 - obs.radius) {
+      obs.y =  Math.floor(Math.random() * ((cvs.height + obs.radius) - obs.radius));
+      obs.x = cvs.width + obs.radius;
+      obs.speed = Math.floor(Math.random() * (obsMaxSpeed - obsMinSpeed) + obsMinSpeed)
+    }
+		if(obs.y + obs.radius > ship.y && obs.y < ship.y + ship.radius && obs.x +
+			obs.radius > ship.x && obs.x < ship.x + ship.radius){
+			console.log('hit');
+		}
+  });
 }
 
 //game loop
 function loop(){
   ctx.beginPath();
-  ctx.rect(0, 0, 800, 500);
+  ctx.rect(0, 0, cvs.width, cvs.height);
   ctx.fillStyle = "Black";
   ctx.fill();
 
-  update();
-  draw();
+
+  if(startPos){
+    for( var i = 0; i < totalObs; i++){
+      makeObs();
+    }
+  startPos = false;
+  }
+
+  drawObs();
+  drawShip();
+  moveShip();
+  moveObs();
+
 
   requestAnimationFrame(loop);
 	}
-loop();
+  loop();
